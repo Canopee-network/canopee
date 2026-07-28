7ef4ddefd38b01c090b655489a8a6f562f45ec08
pierreportal <pierreportal.mac@gmail.com>
2026-07-28 13:53:55 +0200

    feat: connection

--- crates/canopee-network/src/connection.rs
+++ crates/canopee-network/src/connection.rs
@@ -1,18 +1,46 @@
 use crate::message::{Hello, NetworkMessage, Welcome};
+use crate::peer::Peer;
 use crate::protocol::ProtocolVersion;
 use canopee_identity::Identity;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
+pub enum ConnectionState {
+    Handshaking,
+    Connected,
+    Closing,
+}
+
 pub struct Connection {
     stream: TcpStream,
     identity: Arc<Identity>,
+    state: ConnectionState,
+    peer: Option<Peer>,
 }
+
+/* TCP handshake
+
+Node A                    Node B
+   |                        |
+TCP connect                 |
+   |                        |
+   |                        |
+   +---- Hello ------------>|
+   |                        |
+   <--- Welcome ------------|
+   |                        |
+connected                   |
+ */
 
 impl Connection {
     pub fn new(stream: TcpStream, identity: Arc<Identity>) -> Self {
-        Self { stream, identity }
+        Self {
+            stream,
+            identity,
+            state: ConnectionState::Handshaking,
+            peer: None,
+        }
     }
 
     async fn message_loop(&mut self) -> anyhow::Result<()> {
@@ -33,17 +61,31 @@ impl Connection {
     }
 
     async fn handle_message(&mut self, message: NetworkMessage) -> anyhow::Result<()> {
-        if let NetworkMessage::Ping = message {
-            self.send_message(&NetworkMessage::Pong).await?;
+        match message {
+            NetworkMessage::Ping => self.send_message(&NetworkMessage::Pong).await?,
+            // NetworkMessage::...
+            // NetworkMessage::...
+            // ...
+            message => println!("{:?}", message),
         }
+
         Ok(())
     }
+
+    // -----
 
     pub async fn send_message(&mut self, message: &NetworkMessage) -> anyhow::Result<()> {
         let bytes = bincode::serialize(message)?;
         self.stream.write_u32(bytes.len() as u32).await?;
         self.stream.write_all(&bytes).await?;
         Ok(())
+    }
+
+    pub async fn receive_message(&mut self) -> anyhow::Result<NetworkMessage> {
+        let size = self.stream.read_u32().await?;
+        let mut buffer = vec![0; size as usize];
+        self.stream.read_exact(&mut buffer).await?;
+        Ok(bincode::deserialize(&buffer)?)
     }
 
     pub async fn send_hello(&mut self) -> anyhow::Result<()> {
@@ -69,12 +111,7 @@ impl Connection {
         Ok(())
     }
 
-    pub async fn receive_message(&mut self) -> anyhow::Result<NetworkMessage> {
-        let size = self.stream.read_u32().await?;
-        let mut buffer = vec![0; size as usize];
-        self.stream.read_exact(&mut buffer).await?;
-        Ok(bincode::deserialize(&buffer)?)
-    }
+    //------
 
     pub async fn start_outbound(&mut self) -> anyhow::Result<()> {
         self.send_hello().await?;
@@ -82,6 +119,13 @@ impl Connection {
         match self.receive_message().await? {
             NetworkMessage::Welcome(welcome) => {
                 println!("Connected to {}", welcome.identity);
+                self.state = ConnectionState::Connected;
+                self.peer = Some(Peer {
+                    identity: welcome.identity,
+                    connected: true,
+                    protocol_version: welcome.protocol,
+                });
+
                 let _ = self.message_loop().await;
             }
             message => {
